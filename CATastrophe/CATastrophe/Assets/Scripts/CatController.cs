78d181a487e530c00368bb27fde153e322254b02
Jennifer-WW <jenniferwwang47@gmail.com>
2022-01-03 23:49:08 -0500

    Update CatController.cs
    Draft of Cat's WASD/Arrow movement (normalized; no height movements)

--- CATastrophe/CATastrophe/Assets/Scripts/CatController.cs
+++ CATastrophe/CATastrophe/Assets/Scripts/CatController.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class CatController : MonoBehaviour
 {
+    private CharacterController cat;
+    private float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cat = gameObject.AddComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 move;
+
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            move += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            move += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            move += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            move += Vector3.right;
+        }
+
+        cat.Move(move.normalized * speed * Time.deltaTime);
     }
 }
