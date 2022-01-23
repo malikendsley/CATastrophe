421b293a435bb0abaddeeb4f581eb4e1f4f144aa
Jennifer-WW <jenniferwwang47@gmail.com>
2022-01-22 23:28:47 -0500

    Generation for 4 types of buildings instead of only 2

--- CATastrophe/CATastrophe/Assets/Scripts/LSystem/StructureHelper/StructureHelper.cs
+++ CATastrophe/CATastrophe/Assets/Scripts/LSystem/StructureHelper/StructureHelper.cs
@@ -12,8 +12,14 @@ namespace SVS
         public void PlaceStructuresAroundRoad(List<Vector3Int> roadPositions)
         {
             Dictionary<Vector3Int, Direction> freeEstateSpots = FindFreeSpacesAroundRoad(roadPositions);
+            List<Vector3Int> blockedPositions = new List<Vector3Int>();
+
             foreach (var freeSpots in freeEstateSpots)
             {
+                if(blockedPositions.Contains(freeSpots.Key))
+                {
+                    continue;
+                }
                 var rotation = Quaternion.identity;
                 switch (freeSpots.Value)
                 {
@@ -41,19 +47,61 @@ namespace SVS
                     {
                         if (buildingTypes[i].sizeRequired > 1)
                         {
+                            var halfSize = Mathf.CeilToInt(buildingTypes[i].sizeRequired / 2.0f);
+                            List<Vector3Int> tempPositionsBlocked = new List<Vector3Int>();
                             
+                            if(VerifyIfBuildingFits(halfSize, freeEstateSpots, freeSpots, blockedPositions, ref tempPositionsBlocked))
+                            {
+                                blockedPositions.AddRange(tempPositionsBlocked);
+                                var building = SpawnPrefab(buildingTypes[i].GetPrefab(), freeSpots.Key, rotation);
+                                structuresDictionary.Add(freeSpots.Key, building);
+                                foreach(var pos in tempPositionsBlocked)
+                                {
+                                    structuresDictionary.Add(pos, building);
+                                }
+                            }
                         }
                         else
                         {
                             var building = SpawnPrefab(buildingTypes[i].GetPrefab(), freeSpots.Key, rotation);
                             structuresDictionary.Add(freeSpots.Key, building);
-                            
                         }
                         break;
                     }
 
                 }
             }
+        }
+
+        private bool VerifyIfBuildingFits(
+            int halfSize,
+            Dictionary<Vector3Int, Direction> freeEstateSpots,
+            KeyValuePair<Vector3Int, Direction> freeSpots,
+            List<Vector3Int> blockedPositions,
+            ref List<Vector3Int> tempPositionsBlocked)
+        {
+            Vector3Int direction = Vector3Int.zero;
+            if(freeSpots.Value == Direction.Down || freeSpots.Value == Direction.Up)
+            {
+                direction = Vector3Int.right;
+            }
+            else
+            {
+                direction = new Vector3Int(0, 0, 1);
+            }
+            for(int i = 1; i <= halfSize; i++)
+            {
+                var pos1 = freeSpots.Key + direction * i;
+                var pos2 = freeSpots.Key - direction * i;
+                if(!freeEstateSpots.ContainsKey(pos1) || !freeEstateSpots.ContainsKey(pos2) ||
+                    blockedPositions.Contains(pos1) || blockedPositions.Contains(pos2))
+                {
+                    return false;
+                }
+                tempPositionsBlocked.Add(pos1);
+                tempPositionsBlocked.Add(pos2);
+            }
+            return true;
         }
 
         private GameObject SpawnPrefab(GameObject prefab, Vector3Int position, Quaternion rotation)
