1d679de7899d4075885cb55f7719d9e68d1b4607
Malik Endsley <malik.s.endsley@gmail.com>
2022-06-13 02:09:09 -0400

    AI now respect roads, prefab Tiles, next is park + coast

--- CATastrophe/CATastrophe/Assets/Scripts/Agent Locomotion/WanderController.cs
+++ CATastrophe/CATastrophe/Assets/Scripts/Agent Locomotion/WanderController.cs
@@ -16,34 +16,13 @@ public class WanderController : MonoBehaviour
     public float minRange = 10;
     public float maxRange = 20;
 
-    //void OnDrawGizmosSelected()
-    //{
-    //    ExtraGizmos.DrawWireDisk(transform.position, minRange, Color.green);
-    //    ExtraGizmos.DrawWireDisk(transform.position, maxRange, Color.red);
-    
-    //    var nav = GetComponent<NavMeshAgent>();
-    //    if (nav == null || nav.path == null)
-    //        return;
-
-    //    var line = this.GetComponent<LineRenderer>();
-    //    if (line == null)
-    //    {
-    //        line = this.gameObject.AddComponent<LineRenderer>();
-    //        line.material = new Material(Shader.Find("Sprites/Default")) { color = Color.yellow };
-    //        line.startWidth = line.endWidth = 0.5f;
-    //        line.startColor = line.endColor = Color.yellow;
-    //    }
-
-    //    var path = nav.path;
-
-    //    line.positionCount = path.corners.Length;
-
-    //    for (var i = 0; i < path.corners.Length; i++)
-    //    {
-    //        line.SetPosition(i, path.corners[i]);
-    //    }
-
-    //}
+    void OnDrawGizmosSelected()
+    {
+        ExtraGizmos.DrawWireDisk(transform.position, minRange, Color.red);
+        ExtraGizmos.DrawWireDisk(transform.position, maxRange, Color.green);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(agent.destination, 1);
+    }
 
     void OnEnable()
     {
@@ -61,7 +40,7 @@ public class WanderController : MonoBehaviour
     {
         if (agent.pathPending) return;
         if (!(agent.remainingDistance <= agent.stoppingDistance)) return;
-        if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+        if (!agent.hasPath || agent.velocity.sqrMagnitude < 0.1f)
         {
             agent.SetDestination(ChooseRandomLocation());
         }
